ca812217150f879d145d857cb837d3525664d4c1
Jostein Bø Fløystad <jostein.floystad@gmail.com>
2020-03-13 16:24:11 +0100

    Refactor RCForest.

--- rrcforest/forest.py
+++ rrcforest/forest.py
@@ -1,32 +1,50 @@
-from typing import Sequence, Union
+from typing import Sequence, Union, Iterable
 
 from rrcf import rrcf
 import numpy as np
 
 
 class RCForest:
+    _forest: Sequence[rrcf.RCTree]
+    _tree_size: int
+    _point_index: int
 
-    def __init__(self, n_trees=100, tree_size=256):
-        self._trees = [rrcf.RCTree() for _ in range(n_trees)]
+    def __init__(
+            self, n_trees: int = 100, tree_size: int = 256,
+            random_state: Union[None, int, np.random.RandomState] = None):
+        if random_state is None:
+            rng = np.random.RandomState()
+        elif isinstance(random_state, int):
+            rng = np.random.RandomState(random_state)
+        else:
+            rng = random_state
+        max_seed = np.iinfo(np.int32).max
+        self._forest = [
+            rrcf.RCTree(random_state=rng.randint(max_seed))
+            for _ in range(n_trees)
+        ]
         self._tree_size = tree_size
         self._point_index = 0
 
     @property
-    def tree_size(self):
+    def tree_size(self) -> int:
         return self._tree_size
 
     @property
-    def n_trees(self):
-        return len(self._trees)
+    def n_trees(self) -> int:
+        return len(self._forest)
 
-    def initialize(self, data):
-        for point in data:
-            _ = self.insert_point(point)
+    def insert_batch(self, points: Iterable[np.ndarray]):
+        return np.asarray(
+            [self.insert(point) for point in points])
 
-    def insert_point(self, point):
+    def insert(self, point: np.ndarray):
         '''Insert a point into the tree and report its anomaly score.'''
+        if point.ndim != 1:
+            raise ValueError('expected 1D array')
+
         codisp_sum = 0
-        for tree in self._trees:
+        for tree in self._forest:
             if len(tree.leaves) > self.tree_size:
                 tree.forget_point(self._point_index - self.tree_size)
             tree.insert_point(point, index=self._point_index)
